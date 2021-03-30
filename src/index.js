cb86e6adea31487ca32db2df70464ecc89aa46fd
Denis Lafontant <icebox827@gmail.com>
2021-03-30 11:49:42 -0400

    Create getters and setters for ToDo class

--- src/index.js
+++ src/index.js
@@ -1,18 +1,51 @@
 class todo {
-  constructor(title, description, date, priority, check) {
+  constructor(title, desc, date, priority, check) {
     this._title = title;
-    this._descriprion = description;
+    this._desc= desc;
     this._date = date;
     this._priority = priority;
     this._check = check;
   }
 
+  // Getters 
   get title(){
     return this._title;
   }
 
-  get description(){
-    return this._tdescription;
+  get desc(){
+    return this._desc;
+  }
+
+  get date(){
+    return this._date;
   }
  
+  get priority(){
+    return this._priority;
+  }
+
+  get check(){
+    return this._check;
+  }
+
+  //Setters
+  set title() {
+    this._title = title;
+  }
+
+  set desc() {
+    this._desc= desc;
+  }
+
+  set date() {
+    this._date = date;
+  }
+
+  set priority() {
+    this._priority = priority;
+  }
+
+  set check() {
+    this._check = check;
+  }
 }
