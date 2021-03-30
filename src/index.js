965985ab1bba20b9901b20ebe962ef44e318badc
Denis Lafontant <icebox827@gmail.com>
2021-03-30 11:42:15 -0400

    Set style loader

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 class todo {
-  constructor (title, desc, date, priority, check) {
+  constructor(title, description, date, priority, check) {
     this._title = title;
-    this._desc = desc;
+    this._descriprion = description;
     this._date = date;
     this._priority = priority;
     this._check = check;
   }
-  get title (){
-    return this._title
+
+  get title(){
+    return this._title;
   }
-  set title (){
-    this._title = title
+
+  get description(){
+    return this._tdescription;
   }
+ 
 }
