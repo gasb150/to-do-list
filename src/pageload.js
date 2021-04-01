e6c4019c454f1e54f21c295a4660bd50e27f775d
Denis Lafontant <icebox827@gmail.com>
2021-04-01 16:58:52 -0400

    Fix linters for pageload

--- src/pageload.js
+++ src/pageload.js
@@ -1,15 +1,15 @@
-import show from './display'
+import show from './display';
 
 function loadPage () {
-  const projectColumn = document.getElementById('project')
-  const projectList = document.createElement('div')
+  const projectColumn = document.getElementById('project');
+  const projectList = document.createElement('div');
   projectList.addEventListener('click', e => {
-    let eId = e.target.id
-    show.showTask(eId)
+    const eId = e.target.id;
+    show.showTask(eId);
   })
-  show.projects()
-  show.showProjects(projectList)
-  projectColumn.appendChild(projectList)
+  show.projects();
+  show.showProjects(projectList);
+  projectColumn.appendChild(projectList);
 }
 
-export default loadPage
+export default loadPage;
