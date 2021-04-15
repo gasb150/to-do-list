269308bc1e0d432bd4454648986a57f74cfbcd06
tavo <gasb_150@hotmail.com>
2021-04-15 13:13:14 -0500

    Rename project variable

--- test/project.test.js
+++ test/project.test.js
@@ -1,30 +1,12 @@
-<<<<<<< HEAD
-const Project = require('../src/project')
-
-describe ('Project class', () =>{
-    let project;
-    beforeEach(() =>{
-        project = new Project('data')
-    })
-
-
-it ('should create a project', () =>{
-    expect(typeof project).toBe('object')
-})
-
-
-} )
-=======
 const Project = require('../src/project');
 
 describe('Project class', () => {
-  let todo;
+  let project;
   beforeEach(() => {
-    todo = new Project('data');
+    project = new Project('data');
   });
 
   it('should create a project', () => {
-    expect(typeof todo).toBe('object');
+    expect(typeof project).toBe('object');
   });
 });
->>>>>>> 5101bb6e5c9d0d116991f61adaba1e0fd2c46609
